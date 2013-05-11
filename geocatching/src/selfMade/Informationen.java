3c50a206a0bebd8c1ab592c03161b9cc76ba1aa2
Kalle05 <Pascal.kottmann@gmx.de>
2013-05-11 23:29:35 +0200

    Informationen.java korrektur

--- geocatching/src/selfMade/Informationen.java
+++ geocatching/src/selfMade/Informationen.java
@@ -20,13 +20,13 @@ public class Informationen {
 	public String getBundesland() {return bundesland;}
 	public void setBundesland(String bundesland) {this.bundesland = bundesland}
 	
-	public String getprovinz() {return provinz;}
-	public void setprovinz(String provinz) {this.provinz = provinz}
+	public String getProvinz() {return provinz;}
+	public void setProvinz(String provinz) {this.provinz = provinz}
 	
 	public String getBeschreibung_klein() {return beschreibung_klein;}
 	public void setBeschreibung_klein(String beschreibung_klein) {this.beschreibung_klein = beschreibung_klein}
 	
-	public String getbeschreibung_groﬂ() {return beschreibung_groﬂ;}
+	public String getBeschreibung_groﬂ() {return beschreibung_groﬂ;}
 	public void setBeschreibung_groﬂ(String beschreibung_groﬂ) {this.beschreibung_groﬂ = beschreibung_groﬂ}
 	
 	public String getHinweis() {return Hinweis;}
@@ -35,6 +35,6 @@ public class Informationen {
 	public Double getGeschaetzte_zeit() {return geschaetzte_zeit;}
 	public void setGeschaetzte_zeit(String geschaetzte_zeit) {this.geschaetzte_zeit = geschaetzte_zeit}
 	
-	public Double gettravelbug() {return travelbug;}
+	public Double getTravelbug() {return travelbug;}
 	public void setTravelbug(String travelbug) {this.travelbug = travelbug}
 }
