dae47a9cde2878df75a709d9a1413fb0b7c44f24
Odenthma <malte.odenthal@googlemail.com>
2013-06-20 10:05:58 +0200

    LIBS

--- geocatching/src/de/odenthma/geocache/client/Panels/LoginPanel.java
+++ geocatching/src/de/odenthma/geocache/client/Panels/LoginPanel.java
@@ -1,7 +1,14 @@
 package de.odenthma.geocache.client.Panels;
 
+import java.awt.event.ActionListener;
+
 import javax.swing.JPanel;
 
 public class LoginPanel extends JPanel{
+	ActionListener listener;
+	
+	public LoginPanel(ActionListener listener){
+		this.listener = listener;
+	}
 
 }
